5d9c2b58d48a8dcf07b6de85ab21d0c4b70c4725
Betül <69481739+FatmaBetulUyar@users.noreply.github.com>
2023-08-21 17:28:19 +0300

    added new features.

--- gameBoard.js
+++ gameBoard.js
@@ -1,122 +1,189 @@
-const Player = (sign) => {
-  const getSign = () => {
-    return sign;
+document.addEventListener("DOMContentLoaded", function () {
+  const Player = (sign) => {
+    const getSign = () => {
+      return sign;
+    };
+
+    return {
+      getSign,
+    };
   };
 
-  return {
-    getSign,
-  };
-};
+  const gameBoard = (() => {
+    "use strict";
 
-const gameBoard = (() => {
-  "use strict";
+    const boardCells = document.querySelectorAll(".boardCell");
+    const turnInformation = document.getElementById("turnInformation");
+    const scoreXDiv = document.getElementById("scoreX");
+    const scoreODiv = document.getElementById("scoreO");
+    const restartButton = document.getElementById("restartButton");
 
-  const board = document.querySelector(".board");
-  const boardCells = document.querySelectorAll(".boardCell");
-  const turnInformation = document.getElementById("turnInformation");
-  const scoreXDiv = document.getElementById("scoreX");
-  const scoreODiv = document.getElementById("scoreO");
+    const playerX = Player("X");
+    const playerO = Player("O");
+    const winnerMoves = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  const playerX = Player("X");
-  const playerO = Player("O");
-  const winnerMoves = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+    let movesX = [];
+    let movesO = [];
+    let round = 1;
+    let scoreX = 0;
+    let scoreO = 0;
+    let winner = "";
+    let isTurned = true;
+    let isClicked = false;
 
-  let movesX = [];
-  let movesO = [];
-  let round = 1;
-  let scoreX = 0;
-  let scoreO = 0;
-  let winner = "";
-  let isWon = false;
-  const getCurrentPlayerSign = () => {
-    return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
-  };
+    const getCurrentPlayerSign = () => {
+      return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+    };
 
-  boardCells.forEach((element) => {
-    element.addEventListener("click", () => {
-      element.textContent = getCurrentPlayerSign();
-      turnInformation.textContent =
-        "Player " + getCurrentPlayerSign() + "'s Turn";
-      turnInformation.classList.add("turnInformation");
-      round++;
-      findMoves("X");
-      findMoves("O");
-      gameOver();
+    restartButton.addEventListener("click", () => {
+      clearBoard();
+      round = 1;
+      scoreX = 0;
+      scoreO = 0;
+      winner = "";
+      resetScore(scoreXDiv);
+      resetScore(scoreODiv);
     });
-  });
 
-  function clearBoard() {
-    boardCells.forEach((element) => {
-      element.textContent = "";
-    });
-  }
-
-  function gameOver() {
-    if (scoreX === 3 || scoreO === 3) {
-      scoreO === 3 ? (winner = "O") : (winner = "X");
-      turnInformation.textContent = winner + " Won";
-      turnInformation.style.fontWeight = "bold";
+    function play() {
       boardCells.forEach((element) => {
-        element.style.backgroundColor = "lightgray";
-        element.style.pointerEvents = "none";
+        element.addEventListener("click", () => {
+          if (element.textContent === "") {
+            element.textContent = getCurrentPlayerSign();
+          }
+          console.log(getCurrentPlayerSign());
+          turnInformation.textContent = `Player ${getCurrentPlayerSign()}'s Turn`;
+          if (getCurrentPlayerSign() === "X") {
+            playTurn("X", movesX, boardCells, scoreXDiv);
+          } else {
+            playTurn("O", movesO, boardCells, scoreODiv);
+          }
+          round++;
+        });
       });
     }
-  }
+    play();
 
-  function findMoves(player) {
-    if (player === "X") {
-      for (let index = 0; index < 9; index++) {
-        const element = boardCells[index].textContent.valueOf();
-        if (element === player && !movesX.includes(index)) {
-          movesX.push(index);
-          movesX.sort(); // [0, 2, 4, 8]     [0, 4, 8]
-
-          const includesWinnerMove = winnerMoves.some((move) => {
-            return move.every((index) => movesX.includes(index));
-          });
-          if (includesWinnerMove) {
-            clearBoard();
-            scoreX++;
-            isWon = true;
-            movesX = [];
-            scoreXDiv.textContent = scoreX;
-            console.log(scoreX);
-            console.log(player);
-          }
-        }
-      }
-      return movesX;
-    } else {
-      for (let index = 0; index < 9; index++) {
-        const element = boardCells[index].textContent.valueOf();
-        if (element === player && !movesO.includes(index)) {
-          movesO.push(index);
-          movesO.sort();
-
-          const includesWinnerMove = winnerMoves.some((move) => {
-            return move.every((index) => movesO.includes(index));
-          });
-          if (includesWinnerMove) {
-            clearBoard();
-            isWon = true;
-            movesO = [];
-            scoreO++;
-            scoreODiv.textContent = scoreO;
-            console.log(scoreO);
-            console.log(player);
-          }
-        }
-      }
-      return movesO;
+    function clearBoard() {
+      boardCells.forEach((element) => {
+        element.textContent = "";
+        element.style.backgroundColor = "#BEADFA";
+        element.style.pointerEvents = "click";
+      });
+      movesX = [];
+      movesO = [];
     }
-  }
-})();
+
+    function rotateCells() {
+      if (isTurned) {
+        boardCells.forEach((element) => {
+          element.style.transition = "width 2s, height 2s, transform 2s";
+          element.style.transform = "rotate(180deg)";
+          element.style.backgroundColor = "#BEADFA";
+        });
+        isTurned = false;
+      } else {
+        boardCells.forEach((element) => {
+          element.style.transition = "width 2s, height 2s, transform 2s";
+          element.style.transform = "rotate(0deg)";
+          element.style.backgroundColor = "#BEADFA";
+        });
+        isTurned = true;
+      }
+    }
+
+    function gameOver(boardCells) {
+      if (scoreO === 3 || scoreX === 3 || round === 9) {
+        if (scoreO === 3) {
+          winner = "O";
+        }
+        if (scoreX === 3) {
+          winner = "X";
+        }
+        if (!checkBoardCells) {
+          winner = "No Winner";
+          boardCells.forEach((element) => {
+            element.style.backgroundColor = "lightgray";
+            element.style.pointerEvents = "none";
+          });
+        }
+        turnInformation.textContent = winner + " Won";
+        turnInformation.style.fontWeight = "bold";
+      }
+    }
+    function checkBoardCells(boardCells) {
+      return boardCells.every((item) => item.textContent !== "");
+    }
+    function delay(milliseconds) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, milliseconds);
+      });
+    }
+
+    function findWinner(moves) {
+      //[0, 1, 2] [1,4, 0, 2]
+      const winningMove = winnerMoves.find((move) => {
+        return move.every((index) => moves.includes(index));
+      });
+      console.log("wm: ", winningMove);
+      return winningMove;
+    }
+
+    async function setWinnerStyle(winningMove, boardCells) {
+      winningMove.forEach((index) => {
+        boardCells[index].style.backgroundColor = "aquamarine";
+      });
+
+      await delay(2000);
+      clearBoard();
+      rotateCells();
+      gameOver(boardCells);
+    }
+
+    function decreaseScore(player, scoreDiv) {
+      if (player === "X") {
+        scoreX++;
+        scoreDiv.textContent = scoreX;
+      } else {
+        scoreO++;
+        scoreDiv.textContent = scoreO;
+      }
+    }
+
+    function resetScore(scoreDiv) {
+      scoreDiv.textContent = "0";
+    }
+
+    function playTurn(player, moves, boardCells, scoreDiv) {
+      console.log("boardcells", boardCells);
+      for (let index = 0; index < boardCells.length; index++) {
+        const element = boardCells[index].textContent.valueOf();
+
+        if (element === player && !moves.includes(index)) {
+          moves.push(index);
+          moves.sort();
+          let winningMove = findWinner(moves);
+          if (winningMove) {
+            console.log("wm'ye girildi");
+            decreaseScore(player, scoreDiv);
+
+            setWinnerStyle(winningMove, boardCells, scoreDiv);
+
+            winningMove = null;
+            return;
+          }
+        }
+      }
+      return moves;
+    }
+  })();
+});
